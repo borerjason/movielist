0678b07669987f11b8ff04d81c9a55ad1a4f1476
Jason Borer <borerjason@gmail.com>
2017-11-15 10:29:27 -0800

    update airbnb linter

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,56 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import movies from '../../database/data.js'
 import MovieList from './components/MovieList.jsx';
 import AddMovies from './components/AddMovies.jsx';
 import SearchBar from './components/SearchBar.jsx';
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
-    
+  constructor(props) {
+    super(props);
+
     this.state = {
       movies: [],
-      watched: false     
-    }
+      watched: false
+    };
+
+    this.onClickToggleWatched = this.onClickToggleWatched.bind(this);
   }
 
   componentWillMount() {
-    axios.get('/load')
-    .then((result) => {
-      console.log('RESULT IN CLIENT', result)
-      this.setState({ movies: result.data })
-      });
+    axios.get('/load').then((result) => {
+      this.setState({ movies: result.data });
+    });
   }
 
-  onClickUpdateMovieList (value) {
-    let movies = this.state.movies.filter(movie => {
-      return movie.Title.includes(value); 
-    });
+  onClickToggleWatched() {
+    this.setState({ watched: true });
+  }
+
+  onClickToggleWatch() {
+    this.setState({ watched: true });
+  }
+
+  onClickUpdateMovieList(value) {
+    const films = this.state.movies.filter(movie =>
+      movie.Title.includes(value)
+    );
     this.setState({
-      movies: movies
+      movies: films
     });
   }
 
   render() {
     return (
-
       <div>
-      <div id="add-movies">
-        <AddMovies />
+        <div id="add-movies">
+          <AddMovies />
+        </div>
+        <div className="top-container">
+          <div className="watched-buttons">
+            <button className="btn btn-secondary">Watched</button>
+            <button className="btn btn-secondary">To Watch</button>
+          </div>
+          <div className="search-bar">
+            <SearchBar
+              updateMovies={value => this.onClickUpdateMovieList(value)}
+            />
+          </div>
+        </div>
+        <div className="movie-list-container">
+          <MovieList movies={this.state.movies} />
+        </div>
       </div>
-      <div className="search-bar">
-        <SearchBar 
-          updateMovies={(value)=> this.onClickUpdateMovieList(value)}/> 
-      </div>
-      <div className='movie-list-container'>
-       <MovieList
-         movies={this.state.movies}/>
-      </div>
-    </div>
-    
-    )
+    );
   }
 }
 
